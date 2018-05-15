088f95644e1b8087cf1b11d103fb849c67146d47
marc-antoine <marcantoine.bruelhart@gmail.com>
2018-05-15 23:25:49 +0200

    rename bash scripts with file type .sh to use them with the intellij plugin

--- mabs-clock/compose-clock.sh
+++ mabs-clock/compose-clock.sh
@@ -56,4 +56,3 @@ composeMinute
 #set image to the background
 gsettings set org.gnome.desktop.background picture-uri file:///usr/share/backgrounds/r.png
 gsettings set org.gnome.desktop.background picture-options "zoom"
-
