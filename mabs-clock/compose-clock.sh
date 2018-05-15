1cbd7b6c190a9e4b8a1989e96f0b9964b89ce9ce
marc-antoine <marcantoine.bruelhart@gmail.com>
2018-05-15 23:36:14 +0200

    extend gitinore with genereated files

--- mabs-clock/compose-clock.sh
+++ mabs-clock/compose-clock.sh
@@ -5,6 +5,10 @@ then
 	echo "parameter 1: $1"
 fi
 
+#zeiger mitte des haupt bildes
+x=730
+y=1330
+
 rotatePointers() {
 	minute="$(date +%M)"
 	houre="$(date +%H)"
@@ -14,44 +18,37 @@ rotatePointers() {
 		houre=`expr $houre - 12`
 	fi
 
-	mrotation=`expr $minute \* 6`
-	hrotation=`expr $houre \* 30 \+ $minute \/ 4`
+	mrotation=`expr ${minute} \* 6`
+	hrotation=`expr ${houre} \* 30 \+ ${minute} \/ 4`
 
 	#rotieren der zeiger
-	convert -background 'rgba(0,0,0,0)' -rotate $hrotation ./h-original.png ./h.png
-	convert -background 'rgba(0,0,0,0)' -rotate $mrotation ./m-original.png ./m.png
+	convert -background 'rgba(0,0,0,0)' -rotate ${hrotation} ./h-original.png ./h.png
+	convert -background 'rgba(0,0,0,0)' -rotate ${mrotation} ./m-original.png ./m.png
+}
+
+composeHourePointer() {
+    #halbe bild breite
+    hwidth=`convert ./h.png -format "%[fx:w/2]" info:`
+
+    #neu ausrichtung der zeiger zur mitte der uhr
+    xnew=`expr ${x} - ${hwidth} - 28`
+    ynew=`expr ${y} - ${hwidth}`
+    convert ./c-original.png ./h.png -geometry +${xnew}+${ynew} -composite ./r.png
+}
+
+composeMinutePointer() {
+    #halbe bild breite
+    mwidth=`convert ./m.png -format "%[fx:w/2]" info:`
+
+    #neu ausrichtung der zeiger zur mitte der uhr
+    xnew=`expr ${x} - ${mwidth} - 20`
+    ynew=`expr ${y} - ${mwidth} - 10`
+    convert ./r.png ./m.png -geometry +${xnew}+${ynew} -composite ../r.png
 }
 
 rotatePointers
-
-#zeiger mitte des haupt bildes
-x=730
-y=1330
-
-composeHoure() {
-#Stundenzeiger
-#halbe bilder breite
-hwidth=`convert ./h.png -format "%[fx:w/2]" info:`
-
-#neu ausrichtung der zeiger zur mitte der uhr
-xnew=`expr $x - $hwidth - 28`
-ynew=`expr $y - $hwidth`
-convert ./c-original.png ./h.png -geometry +$xnew+$ynew -composite ./r.png
-}
-
-composeHoure
-
-composeMinute() {
-#Minutenzeiger
-#halbe bilder breite
-mwidth=`convert ./m.png -format "%[fx:w/2]" info:`
-
-#neu ausrichtung der zeiger zur mitte der uhr
-xnew=`expr $x - $mwidth - 20`
-ynew=`expr $y - $mwidth - 10`
-convert ./r.png ./m.png -geometry +$xnew+$ynew -composite ../r.png
-}
-composeMinute
+composeHourePointer
+composeMinutePointer
 
 #set image to the background
 gsettings set org.gnome.desktop.background picture-uri file:///usr/share/backgrounds/r.png
