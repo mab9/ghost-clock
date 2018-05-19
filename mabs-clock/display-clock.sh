1d28cc0d20a8b05b21b95494123ae3e33bd55357
marc-antoine <marcantoine.bruelhart@gmail.com>
2018-05-19 17:10:12 +0200

    remove unused script that was fully replaced in the compose-clock script2

--- mabs-clock/display-clock.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash          
-
-#set image to the background
-gsettings set org.gnome.desktop.background picture-uri file:///usr/share/backgrounds/r.png
-gsettings set org.gnome.desktop.background picture-options "zoom"
-
-
